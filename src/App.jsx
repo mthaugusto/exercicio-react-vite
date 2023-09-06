44244c4210f5fd67b320563be07ab6be63d9e379
mthaugusto <mthaugusto94@icloud.com>
2023-09-05 23:00:19 -0300

    Corrigindo import Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import { Outlet } from 'react-router-dom'
-import Menu from './components/Menu'
-import Rodape from './components/Rodape'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Menu from "./components/menu";
+Outlet;
+import Rodape from "./components/Rodape";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <Menu/>
-      <Outlet/>
-      <Rodape/>
-    </>
-  )
+	return (
+		<>
+			<Menu />
+			<Outlet />
+			<Rodape />
+		</>
+	);
 }
 
-export default App
+export default App;

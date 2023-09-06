9b067b33c1f8581e1c78b72bff59c5aa4b17a757
mthaugusto <mthaugusto94@icloud.com>
2023-09-05 22:18:41 -0300

    Adicionando componentes

---
+++ src/routes/Produtos/Index.jsx
@@ -0,0 +1,20 @@
+import { listaProdutos } from "../../components/listaProdutos";
+import { Link } from "react-router-dom";
+
+export default function Produtos() {
+
+    return (
+      <>
+          <main>
+              <h1>Produtos</h1>
+              {listaProdutos.map(prod => (
+                <div key={prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar o produto {prod.nome}
+                    </Link>
+                </div>
+              ))}
+          </main>
+      </>
+    )
+}
